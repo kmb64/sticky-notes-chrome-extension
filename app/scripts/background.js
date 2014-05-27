e20fc1721329e2aa348e720c408056d4256cca64
Karl Balemi <karl.balemi@auspost.com.au>
2014-05-27 18:09:36 +1000

    Adding missing scripts and removing commented code.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -6,16 +6,6 @@ chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-//chrome.browserAction.setBadgeText({text: '\'kk'});
-
 chrome.browserAction.onClicked.addListener(function () {
   chrome.tabs.executeScript(null, {file: 'scripts/app.js'});
 });
-
-//chrome.tabs.onActivated.addListener(function(){
-//  chrome.tabs.query('active', function(active) {
-//    if(active){
-//      chrome.tabs.executeScript(null, 'alert("hello")');
-//    }
-//  });
-//});
