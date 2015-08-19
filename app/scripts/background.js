e7245cc84c9c54c31bdac8db96678b38b92268df
Karl Balemi <karl.balemi@auspost.com.au>
2015-08-19 12:54:57 +1000

    Remove badge text.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -6,8 +6,6 @@ chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'K'});
-
 chrome.browserAction.onClicked.addListener(function () {
   chrome.tabs.executeScript(null, {file: 'scripts/app.js'});
   console.log('init script loaded');
